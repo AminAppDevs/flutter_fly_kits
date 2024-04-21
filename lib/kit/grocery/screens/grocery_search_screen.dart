2b9ecc587915784dbe069f0c8c3b130d51c4f407
AminAppDevs <151901571+lmma-app@users.noreply.github.com>
2024-04-21 09:59:28 +0300

    new

--- lib/kit/grocery/screens/grocery_search_screen.dart
+++ lib/kit/grocery/screens/grocery_search_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fly_kits/kit/grocery/components/grocery_text_form_field.dart';
 
 class GrocerySearchScreen extends StatelessWidget {
   const GrocerySearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(),
+      ),
+    );
   }
 }
