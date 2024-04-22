448ab2f206b36f94a41a863adb039954a9c3cf1e
amin <amin.app.devs@gmail.com>
2024-04-22 17:59:13 +0300

    new

--- lib/kit/blog/components/post/blog_create_post_form.dart
+++ lib/kit/blog/components/post/blog_create_post_form.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fly_kits/kit/blog/components/blog_form_filed.dart';
 import 'package:flutter_fly_kits/utils/spacing.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:ionicons/ionicons.dart';
 
 class BlogCreatePostForm extends StatelessWidget {
   BlogCreatePostForm({super.key});
   final formKey = GlobalKey<FormState>();
+  final QuillController controller = QuillController.basic();
 
   final TextEditingController titleController = TextEditingController();
 
@@ -20,6 +22,30 @@ class BlogCreatePostForm extends StatelessWidget {
           hintText: "title",
         ),
         vertical(15),
+        QuillToolbar.simple(
+          configurations: QuillSimpleToolbarConfigurations(
+            controller: controller,
+            buttonOptions: QuillSimpleToolbarButtonOptions(
+              search: null
+            ),
+            sharedConfigurations: const QuillSharedConfigurations(
+              locale: Locale('en'),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: QuillEditor.basic(
+            configurations: QuillEditorConfigurations(
+              controller: controller,
+              readOnly: false,
+              customStyles: DefaultStyles(),
+              sharedConfigurations: const QuillSharedConfigurations(
+                locale: Locale('en'),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
