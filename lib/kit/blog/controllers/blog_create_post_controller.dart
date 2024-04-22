4367b5583f992c8fa35c096fc5199e4122454fee
amin <amin.app.devs@gmail.com>
2024-04-22 14:37:57 +0300

    new

--- lib/kit/blog/controllers/blog_create_post_controller.dart
+++ lib/kit/blog/controllers/blog_create_post_controller.dart
@@ -1,23 +1,6 @@
 import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
 
 class BlogCreatePostController extends GetxController {
-  XFile? image;
   // upload image
-  void uploadImage({required bool fromGallery}) async {
-    final ImagePicker picker = ImagePicker();
-    final LostDataResponse response = await picker.retrieveLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (fromGallery) {
-      final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      this.image = image;
-      update();
-    } else {
-      final XFile? image = await picker.pickImage(source: ImageSource.camera);
-      this.image = image;
-      update();
-    }
-  }
+  void uploadImage({required bool fromGallery}) async {}
 }
